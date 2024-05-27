0f61ab957eed4a82b4cf846e8ca2cbe42ca621ea
Luke Sloane-Bulger <luke.sloanebulger@protonmail.com>
2024-05-27 19:13:12 +0100

    main - update

--- lukelifts/pages/_app.tsx
+++ lukelifts/pages/_app.tsx
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../public/css/globals.css';
 import { AppProps } from 'next/app';
 import { useEffect } from 'react';
-import Head from 'next/head';
 import Script from 'next/script';
 
 // Require jQuery
@@ -23,20 +22,15 @@ if (typeof window !== 'undefined') {
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     require('bootstrap/dist/js/bootstrap.bundle.min.js');
-        /* <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> */
-  }, [])
+  }, []);
 
   return (
     <>
-      {/* <Head> */}
         {/* <!-- Bootstrap JS and dependencies --> */}
         <Script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" defer/>
-        {/* <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script> */}
         <Script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" defer/>
-      {/* </Head> */}
       <Component {...pageProps} />;
     </>
-
   )
 }
 
--- lukelifts/pages/_document.tsx
+++ lukelifts/pages/_document.tsx
@@ -4,21 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-        /> */}
-        {/* <link rel="stylesheet" href="css/globals.css" /> */}
       </Head>
       <body>
-        {/* <div className="full-screen-container"> */}
           <Main />
-        {/* </div> */}
         <NextScript />
-        {/* <!-- Bootstrap JS and dependencies --> */}
-        {/* <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> */}
-        {/* <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script> */}
-        {/* <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> */}
       </body>
     </Html>
   );
