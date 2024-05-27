2bfbc25a9679df536aff36e378f005bb9cc9a358
Luke Sloane-Bulger <luke.sloanebulger@protonmail.com>
2024-05-27 18:37:33 +0100

    main - update

---
+++ lukelifts/out/_next/static/5iFHcHx2qW34uG4ls6Ook/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-f71194c9498a8be6.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
