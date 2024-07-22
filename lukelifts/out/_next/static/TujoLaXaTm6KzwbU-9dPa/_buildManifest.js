b18d09096129c25884dc8fcc425b93c7d5eb1f5e
Luke Sloane-Bulger <luke.sloanebulger@protonmail.com>
2024-07-22 08:11:43 +0100

    main - update prices

---
+++ lukelifts/out/_next/static/TujoLaXaTm6KzwbU-9dPa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/664-806b52bf69b97e98.js","static/chunks/pages/index-c7722e8d3fa3a6d6.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
